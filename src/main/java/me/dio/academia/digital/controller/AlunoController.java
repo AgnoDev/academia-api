e45703ef5c0288e0645ae3fabdbc6e93a466e0e8
Agno <agnodev87@gmail.com>
2023-05-22 20:41:14 -0300

    implementa getAll no controller

--- src/main/java/me/dio/academia/digital/controller/AlunoController.java
+++ src/main/java/me/dio/academia/digital/controller/AlunoController.java
@@ -1,9 +1,24 @@
 package me.dio.academia.digital.controller;
 
+import me.dio.academia.digital.entity.Aluno;
+import me.dio.academia.digital.service.impl.AlunoServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 @RequestMapping("/alunos")
 public class AlunoController {
+    @Autowired
+    private AlunoServiceImpl service;
+
+    @GetMapping
+    private List<Aluno> getAll(){
+        return service.getAll();
+    }
+
 }
+
+
