81a5339d39b2b007ed6effef20fc61c1dcd36891
Agno <agnodev87@gmail.com>
2023-05-22 09:58:32 -0300

    configura teste API Home (classe Main)

--- src/main/java/me/dio/academia/digital/AcademiaDigitalApplication.java
+++ src/main/java/me/dio/academia/digital/AcademiaDigitalApplication.java
@@ -2,9 +2,19 @@ package me.dio.academia.digital;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class AcademiaDigitalApplication {
+
+	@GetMapping
+	public String getHomeTest(){
+		return "Academia SouFit - Testando API Home";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AcademiaDigitalApplication.class, args);
