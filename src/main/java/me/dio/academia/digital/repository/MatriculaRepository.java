2fb86f29af639a410a7642f60119c283fbb64ccd
Agno <agnodev87@gmail.com>
2023-05-23 11:22:23 -0300

    implementa MatriculaRepository

--- src/main/java/me/dio/academia/digital/repository/MatriculaRepository.java
+++ src/main/java/me/dio/academia/digital/repository/MatriculaRepository.java
@@ -1,4 +1,9 @@
 package me.dio.academia.digital.repository;
 
-public interface MatriculaRepository {
+import me.dio.academia.digital.entity.Matricula;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MatriculaRepository extends JpaRepository<Matricula, Long> {
 }
--- src/main/java/me/dio/academia/digital/service/impl/MatriculaServiceImpl.java
+++ src/main/java/me/dio/academia/digital/service/impl/MatriculaServiceImpl.java
@@ -1,12 +1,18 @@
 package me.dio.academia.digital.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import me.dio.academia.digital.entity.Matricula;
 import me.dio.academia.digital.entity.form.MatriculaForm;
+import me.dio.academia.digital.repository.MatriculaRepository;
 import me.dio.academia.digital.service.IMatriculaService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class MatriculaServiceImpl implements IMatriculaService {
+    private final MatriculaRepository matriculaRepository;
     @Override
     public Matricula create(MatriculaForm form) {
         return null;
