690f86da3d0bde107586581fb5265b71ade151c6
Agno <agnodev87@gmail.com>
2023-05-23 08:12:27 -0300

    Inj Dep e implementa metodos

---
+++ src/main/java/me/dio/academia/digital/service/impl/AlunoServiceImpl.java
@@ -0,0 +1,44 @@
+package me.dio.academia.digital.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import me.dio.academia.digital.entity.Aluno;
+import me.dio.academia.digital.entity.form.AlunoForm;
+import me.dio.academia.digital.entity.form.AlunoUpdateForm;
+import me.dio.academia.digital.repository.AlunoRepository;
+import me.dio.academia.digital.service.IAlunoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AlunoServiceImpl implements IAlunoService {
+    private final AlunoRepository alunoRepository;
+
+
+    @Override
+    public Aluno create(AlunoForm form) {
+        return null;
+    }
+
+    @Override
+    public Aluno get(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Aluno> getAll() {
+        return alunoRepository.findAll();
+    }
+
+    @Override
+    public Aluno update(Long id, AlunoUpdateForm formUpdate) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
