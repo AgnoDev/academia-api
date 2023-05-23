bdf3868b467ea2aaf6690463f6c6c39f938fb976
Agno <agnodev87@gmail.com>
2023-05-23 10:29:36 -0300

    implementa metodo getAllAvaliacoesById

--- src/main/java/me/dio/academia/digital/controller/AlunoController.java
+++ src/main/java/me/dio/academia/digital/controller/AlunoController.java
@@ -2,9 +2,9 @@ package me.dio.academia.digital.controller;
 
 import lombok.RequiredArgsConstructor;
 import me.dio.academia.digital.entity.Aluno;
+import me.dio.academia.digital.entity.AvaliacaoFisica;
 import me.dio.academia.digital.entity.form.AlunoForm;
 import me.dio.academia.digital.service.impl.AlunoServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,6 +23,12 @@ public class AlunoController {
     @PostMapping
     public Aluno create (@RequestBody AlunoForm alunoForm){
         return alunoService.create(alunoForm);
+    }
+
+    @GetMapping("/avaliacoes/{id}")
+    public List<AvaliacaoFisica> getAllAvaliacoesById(@PathVariable Long id) {
+        return alunoService.getAllAvaliacoesById(id);
+
     }
 
 
