160d694ef31faab7f8cc8b323400bdb05e6db174
Agno <agnodev87@gmail.com>
2023-05-23 11:03:21 -0300

    update anotations

--- src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
@@ -5,10 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.br.CPF;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Data
@@ -16,15 +13,15 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class AlunoForm {
 
-  @NotBlank(message = "Preenchimento obrigatório.")
+  @NotEmpty(message = "Preenchimento obrigatório.")
   @Size(min = 3, max = 50, message = "'${validatedValue}' precisa estar entre {min} e {max} caracteres.")
   private String nome;
 
-  @NotBlank(message = "Preenchimento obrigatório.")
+  @NotEmpty(message = "Preenchimento obrigatório.")
   @CPF(message = "'${validatedValue}' é inválido.")
   private String cpf;
 
-  @NotBlank(message = "Preenchimento obrigatório.")
+  @NotEmpty(message = "Preenchimento obrigatório.")
   @Size(min = 3, max = 50, message = "'${validatedValue}' precisa estar entre {min} e {max} caracteres.")
   private String bairro;
 
