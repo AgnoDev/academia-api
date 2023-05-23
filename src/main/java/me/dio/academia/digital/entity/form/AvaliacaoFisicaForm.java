3cc5c4cb165dfbb482014b9ec26e8d1cf960503b
Agno <agnodev87@gmail.com>
2023-05-23 10:59:50 -0300

    anotations AvaliacaoFisicaForm

--- src/main/java/me/dio/academia/digital/entity/form/AvaliacaoFisicaForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/AvaliacaoFisicaForm.java
@@ -5,14 +5,24 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import me.dio.academia.digital.entity.Aluno;
 
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class AvaliacaoFisicaForm {
 
+  @Positive(message = "Id do Aluno deve ser maior que 0(zero).")
   private Long alunoId;
 
+  @NotNull(message = "Preenchimento obrigatório.")
+  @Positive(message = "O peso do Aluno deve ser maior que 0(zero).")
   private double peso;
 
+  @NotNull(message = "Preenchimento obrigatório.")
+  @Positive(message = "A altura do Aluno deve ser maior que 0(zero).")
+  @DecimalMin(value = "150", message = "'${validatedValue}' precisa ser no mínimo {value}.")
   private double altura;
 }
