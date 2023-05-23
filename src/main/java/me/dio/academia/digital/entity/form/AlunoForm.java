960639e3f38429b868b725bb0a3c0dcc0f6a1c0c
Agno <agnodev87@gmail.com>
2023-05-23 10:50:45 -0300

    anotations AlunoForm e MatriculaForm

--- src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
@@ -3,7 +3,12 @@ package me.dio.academia.digital.entity.form;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Data
@@ -11,11 +16,19 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class AlunoForm {
 
+  @NotBlank(message = "Preenchimento obrigatório.")
+  @Size(min = 3, max = 50, message = "'${validatedValue}' precisa estar entre {min} e {max} caracteres.")
   private String nome;
 
+  @NotBlank(message = "Preenchimento obrigatório.")
+  @CPF(message = "'${validatedValue}' é inválido.")
   private String cpf;
 
+  @NotBlank(message = "Preenchimento obrigatório.")
+  @Size(min = 3, max = 50, message = "'${validatedValue}' precisa estar entre {min} e {max} caracteres.")
   private String bairro;
 
+  @NotNull(message = "Preenchimento obrigatório.")
+  @Past(message = "Data '${validatedValue}' é inválida.")
   private LocalDate dataDeNascimento;
 }
--- src/main/java/me/dio/academia/digital/entity/form/MatriculaForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/MatriculaForm.java
@@ -1,7 +1,19 @@
 package me.dio.academia.digital.entity.form;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MatriculaForm {
 
+  @NotNull(message = "Preenchimento obrigatório.")
+  @Positive(message = "Id do Aluno deve ser maior que 0(zero).")
   private Long alunoId;
 
 }
